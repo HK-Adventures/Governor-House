8b7364cb242e18a962b1e8daca3cf8d6242f8c28
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-03-26 12:49:16 +0500

    Add files via upload

---
+++ Task 6 Stripping Names/app.js
@@ -0,0 +1,4 @@
+var Namewithwhitespace = "\n\t Hassaan Khan \t\n";
+console.log(Namewithwhitespace);
+var Namewithwhiteoutspace = Namewithwhitespace.trim();
+console.log(Namewithwhiteoutspace);
---
+++ Task 6 Stripping Names/app.ts
@@ -0,0 +1,4 @@
+let Namewithwhitespace: string = "\n\t Hassaan Khan \t\n";
+console.log(Namewithwhitespace);
+let Namewithwhiteoutspace = Namewithwhitespace.trim();
+console.log(Namewithwhiteoutspace);

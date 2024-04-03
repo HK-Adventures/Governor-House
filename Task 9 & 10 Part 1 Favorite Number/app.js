6355ae6b44250e8681fc37e2ce3184a2e1a2cffd
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-04-03 13:44:14 +0500

    Add files via upload

---
+++ Task 9 & 10 Part 1 Favorite Number/app.js
@@ -0,0 +1,4 @@
+var FavoriteNumber = 8;
+var Message = "My favorite number is ".concat(FavoriteNumber);
+console.log(Message);
+//this program reveals my favorite number 
---
+++ Task 9 & 10 Part 1 Favorite Number/app.ts
@@ -0,0 +1,5 @@
+const FavoriteNumber: number = 8;
+const Message: string = `My favorite number is ${FavoriteNumber}`;
+console.log(Message);
+
+//this program reveals my favorite number 

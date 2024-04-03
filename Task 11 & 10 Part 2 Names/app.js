28b170d92f88953ccc1f5531d32c15fd4d5eb339
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-04-03 13:49:04 +0500

    Add files via upload

---
+++ Task 11 & 10 Part 2 Names/app.js
@@ -0,0 +1,5 @@
+//this programs is about storing my friend's name in array and printing them one by one
+var names = ["Ahmed", "Zohaib", "Mousab"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
---
+++ Task 11 & 10 Part 2 Names/app.ts
@@ -0,0 +1,6 @@
+//this programs is about storing my friend's name in array and printing them one by one
+
+let names: string[] = ["Ahmed" , "Zohaib" , "Mousab"];
+ console.log(names[0]);
+ console.log(names[1]);
+ console.log(names[2]);

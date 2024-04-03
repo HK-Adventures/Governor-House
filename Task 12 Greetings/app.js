ff70b8260c766de70efb09c7e42c0b0f6958bd32
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-04-03 13:51:12 +0500

    Add files via upload

---
+++ Task 12 Greetings/app.js
@@ -0,0 +1,4 @@
+var names = ["Ahmed", "Zohaib", "Mousab"];
+console.log("Focus on your studies ".concat(names[0]));
+console.log("Focus on your studies ".concat(names[1]));
+console.log("Focus on your studies ".concat(names[2]));
---
+++ Task 12 Greetings/app.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Ahmed" , "Zohaib" , "Mousab"];
+
+console.log(`Focus on your studies ${names[0]}`);
+console.log(`Focus on your studies ${names[1]}`);
+console.log(`Focus on your studies ${names[2]}`);

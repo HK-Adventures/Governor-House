3c45977095666397687fd490fa6e0b72fedfda90
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-04-03 13:53:49 +0500

    Add files via upload

---
+++ Task 13 Your Own Array/app.js
@@ -0,0 +1,4 @@
+var transportation = ["Mazda RX8", "Ford Raptor", "Yahama R1000"];
+console.log("I would like to own a ".concat(transportation[0]));
+console.log("I would like to own a ".concat(transportation[1]));
+console.log("I would like to own a ".concat(transportation[2]));
---
+++ Task 13 Your Own Array/app.ts
@@ -0,0 +1,5 @@
+let transportation: string[] = ["Mazda RX8", "Ford Raptor" , "Yahama R1000"];
+
+console.log(`I would like to own a ${transportation[0]}`);
+console.log(`I would like to own a ${transportation[1]}`);
+console.log(`I would like to own a ${transportation[2]}`);

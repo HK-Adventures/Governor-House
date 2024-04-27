4c88c33821775b61adefc5d0475afeed513d30e8
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-04-27 12:10:23 +0500

    Add files via upload

---
+++ Task 19 Dinner Guests/app.js
@@ -0,0 +1,7 @@
+var GuestList = ["Ahmed", "Ashir", "Zohaib", "Muneeb", "Mustajab"];
+// console.log ("\n List Of Invitations : \n");
+// for ( let a=0; a < GuestList.length; a++)
+// {
+//     console.log (`Dear ${GuestList[a]} : \n You are invited to dinner. \n`);
+// }
+console.log("Total number of people Invited : ".concat(GuestList.length));
---
+++ Task 19 Dinner Guests/app.ts
@@ -0,0 +1,10 @@
+let GuestList : string[] = ["Ahmed" , "Ashir" , "Zohaib" , "Muneeb" , "Mustajab"];
+
+// console.log ("\n List Of Invitations : \n");
+
+// for ( let a=0; a < GuestList.length; a++)
+// {
+//     console.log (`Dear ${GuestList[a]} : \n You are invited to dinner. \n`);
+// }
+
+ console.log(`Total number of people Invited : ${GuestList.length}`);

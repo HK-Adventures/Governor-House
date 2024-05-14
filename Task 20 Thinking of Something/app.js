a08c4764164ca647cb1511f9483fbe06f6d1a04f
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-05-14 12:07:33 +0500

    Add files via upload

---
+++ Task 20 Thinking of Something/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Locations = ["Mount Fuji", "River Indus", "Austin", "Germany"];
+console.log("\t\nList Of Selected Locations ");
+for (let i = 0; i < Locations.length; i++) {
+    console.log(Locations[i]);
+}
---
+++ Task 20 Thinking of Something/app.ts
@@ -0,0 +1,8 @@
+const Locations: string[] = ["Mount Fuji" , "River Indus" , "Austin" , "Germany"];
+
+console.log("\t\nList Of Selected Locations ");
+
+for (let i = 0; i < Locations.length; i++) {
+    console.log(Locations[i]);
+}
+

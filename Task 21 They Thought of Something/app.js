410951f3fb3064ec0a0eaead99c244572f5059d5
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-05-14 11:59:56 +0500

    Add files via upload

---
+++ Task 21 They Thought of Something/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let object = { Name: "Hassaan Khan", Class: 12, Age: 18, ownhouse: false };
+console.log("\nBelow is the Information Stroed In Object \n");
+console.log(`My Name is ${object.Name}`);
+console.log(`I'm ${object.Age} Years old`);
+console.log(`I Study in class ${object.Class}`);
+console.log(`Do I own a House? \nThe answer is ${object.ownhouse}`);

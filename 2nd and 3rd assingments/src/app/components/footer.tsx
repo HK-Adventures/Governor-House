6b1a1033c59b706249e626329da8b993feefda09
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-10-02 13:06:05 +0500

    Delete 2nd and 3rd assingments directory

--- 2nd and 3rd assingments/src/app/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function footer () {
-    return (
-        <div className="footerfirst">
-            <div className="footersecond">
-                
-            </div>
-        </div>
-    );
-}
